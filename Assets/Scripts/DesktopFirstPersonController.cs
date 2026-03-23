ab7e87f75790e19cba5b29149f5ecb909da215cc
FredBrumati <brumatifrederico@gmail.com>
2026-03-23 16:09:50 -0300

    Player config

--- Assets/Scripts/DesktopFirstPersonController.cs
+++ Assets/Scripts/DesktopFirstPersonController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 [RequireComponent(typeof(CharacterController))]
 public class DesktopFirstPersonController : MonoBehaviour
 {
@@ -6,39 +7,51 @@ public class DesktopFirstPersonController : MonoBehaviour
     public float mouseSensitivity = 2f;
     public float gravity = -9.81f;
     public Transform cameraTransform;
+
     private CharacterController controller;
     private float verticalVelocity;
     private float cameraPitch = 0f;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+
     void Update()
     {
+        Debug.Log("Rodando");
         Look();
         Move();
     }
+
     void Look()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
         transform.Rotate(Vector3.up * mouseX);
+
         cameraPitch -= mouseY;
         cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+
         cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
     }
+
     void Move()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * moveSpeed * Time.deltaTime);
+
         if (controller.isGrounded && verticalVelocity < 0)
         {
             verticalVelocity = -2f;
         }
+
         verticalVelocity += gravity * Time.deltaTime;
         Vector3 gravityMove = Vector3.up * verticalVelocity;
         controller.Move(gravityMove * Time.deltaTime);
